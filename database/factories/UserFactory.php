3008175ff17a56533d17277b156bf6484be46e25
Luis Eduardo Alcantara Olvera <geekshinobi@gmail.com>
2020-03-03 12:49:27 -0600

    Creamos un modelo, una migracion y un seeder con su facoty y editamos la ruta de apis

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
+use App\Post;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -24,5 +25,12 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->title,
+        'body' => $faker->text(100),
     ];
 });
